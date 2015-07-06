b1388d510aa3a9299002409fdbc3f74a49e87172
Jan Dohl <jan.dohl@airrays.com>
2015-07-06 12:52:40 +0200

    libbladeRF: Added RX/TX trigger support

--- host/libraries/libbladeRF/include/libbladeRF.h
+++ host/libraries/libbladeRF/include/libbladeRF.h
@@ -3439,6 +3439,80 @@ API_EXPORT
 int CALL_CONV bladerf_calibrate_dc(struct bladerf *dev,
                                    bladerf_cal_module module);
 
+/**
+ * Trigger control register "Arm" bit
+ *
+ * This bit arms (i.e., enables) the trigger controller when set to 1. Samples
+ * will be gated until the "Fire" bit has been asserted.
+ *
+ * A 0 in this bit disables the trigger controller. Samples will continue
+ * to flow as they normally do in this state.
+ */
+#define BLADERF_TRIGGER_REG_ARM     ((uint8_t) (1 << 0))
+
+/**
+ * Trigger control register "Fire" bit
+ *
+ * For a master, this bit causes a trigger to be sent to all slave devices. Once
+ * this trigger is received (the master "receives" it immediately as well),
+ * devices begin streaming samples.
+ *
+ * This bit has no effect on slave devices.
+ */
+#define BLADERF_TRIGGER_REG_FIRE    ((uint8_t) (1 << 1))
+
+/**
+ * Trigger control register "Master" bit
+ *
+ * Selects whether the device is a trigger master (1) or trigger slave (0).
+ * The trigger master drives the trigger signal as an output.
+ * Slave devices configure the trigger signal as an input.
+ */
+#define BLADERF_TRIGGER_REG_MASTER  ((uint8_t) (1 << 2))
+
+/**
+ * Trigger control registers "line" bit
+ *
+ * This is a read-only register bit that denotes the current state of the
+ * the trigger signal.
+ */
+#define BLADERF_TRIGGER_REG_LINE    ((uint8_t) (1 << 3))
+
+/**
+ * Read trigger control register
+ *
+ * @param   dev         Device handle
+ * @param   module      Module to read from
+ * @param   trigger     Trigger to read from
+ * @param   val         Pointer to variable that register is read into
+ *                      See the BLADERF_TRIGGER_REG_* macros for the meaning
+ *                      of each bit.
+ *
+ * @return 0 on success, value from \ref RETCODES list on failure
+ */
+API_EXPORT
+int CALL_CONV bladerf_read_trigger(struct bladerf *dev,
+                                   bladerf_module module,
+                                   bladerf_trigger trigger,
+                                   uint8_t *val);
+
+/**
+ * Write trigger control register
+ *
+ * @param   dev         Device handle
+ * @param   module      Module to configure
+ * @param   trigger     Triger to configure
+ * @param   val         Data to write into the trigger control register.
+ *                      See the BLADERF_TRIGGER_REG_* macros for options.
+ *
+ * @return 0 on success, value from \ref RETCODES list on failure
+ */
+API_EXPORT
+int CALL_CONV bladerf_write_trigger(struct bladerf *dev,
+                                    bladerf_module module,
+                                    bladerf_trigger trigger,
+                                    uint8_t val);
+
 /** @} (End of LOW_LEVEL) */
 
 /**
